353ec9a01049b6f095c140f50968d22f1c16cab0
PhanVietHieu <22641741.hieu@student.iuh.edu.vn>
2025-11-03 12:34:01 +0700

    Update config Trang chi tiet san pham

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+//import HomeScreen from './screens/HomeScreen';
 import { ProductDetailScreen } from "./src/screens/ProductDetailScreen";
 
 export default function App() {
-  return <ProductDetailScreen />;
+  return (
+    <View style={styles.container}>
+      {/* <HomeScreen /> */}
+      <ProductDetailScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({

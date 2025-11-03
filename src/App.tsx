df48396c6f8babc63ac1414bb1d6e92a5b0013bb
PhanVietHieu <22641741.hieu@student.iuh.edu.vn>
2025-11-03 12:37:38 +0700

    Last update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //import HomeScreen from './screens/HomeScreen';
-import { ProductDetailScreen } from "./src/screens/ProductDetailScreen";
+import { ProductDetailScreen } from "./screens/ProductDetailScreen";
 
 export default function App() {
   return (
